0a7855c9a8ec7ebc37022c9d8416b21a35d767c8
Vaibhav Diwan <vaibhav.diwan@niit.com>
2022-09-27 21:19:32 +0530

    completed the code

--- src/StudentMain.java
+++ src/StudentMain.java
@@ -12,8 +12,8 @@ Display the following menu to user,
 public class StudentMain {
     public static void main(String[] args) {
         // menu
-        // storage for storing Student object
-        //don't know
+        // storage for storing Student object --> an array --> of type(primitive or non-primitive) Student
+        //don't know --> size we don't --> we are assuming 100 student data we will get
         // max size as 100 -> the size of student array is 100
         //count of student object
         final int MAX_SIZE = 100;
@@ -26,23 +26,26 @@ public class StudentMain {
         //for, while , do while
         int choice;
         do {
-            System.out.println("Slect form following choices");
+            System.out.println("Select form following choices");
             System.out.println("press 1. to add details of new student");
             System.out.println("press 2 to display all the entered student");
             System.out.println("press 3 to exit");
             choice = scanner.nextInt();
+            scanner.nextLine();
             switch (choice){
                 case 1: // do it by calling a method to preform case 1
                         // do it here -- using this way
                     Student object = new Student();
                     System.out.println("enter name");
-                    object.name = scanner.nextLine();
+                    object.name = scanner.nextLine(); //vaibhav diwan
                     System.out.println("enter age");
                     object.age = scanner.nextInt();
+                    scanner.nextLine();
                     System.out.println("enter email");
                     object.email = scanner.nextLine();
                     System.out.println("enter mobile");
                     object.mobile = scanner.nextLong();
+                    scanner.nextLine();
                     System.out.println("enter address");
                     object.address = scanner.nextLine();
 
@@ -51,7 +54,7 @@ public class StudentMain {
                     count ++;
                     break;
                 case 2:
-                    //display all the element of students array
+                    //display all the element of student array
                     display(students, count);
                     break;
                 case 3:
@@ -65,7 +68,7 @@ public class StudentMain {
         } while (choice != 3);
     }
     public static void display(Student[] array,int noOfObjects){
-        for (int i = 0; i <= noOfObjects; i++) {
+        for (int i = 0; i < noOfObjects; i++) {
             Student object = array[i];
             System.out.println("object.name = " + object.name);
             System.out.println("object.age = " + object.age);
