5437a339dee3544768ed8eae63f33782571e67e8
Vaibhav Diwan <vaibhav.diwan@niit.com>
2022-09-28 20:53:57 +0530

    modified the code for further discussion

--- src/Student.java
+++ src/Student.java
@@ -8,13 +8,31 @@ Display the following menu to user,
 3. to exit
  */
 public class Student {
-    String name;//name of variable starts with lower cases character, if more than one words
+    //ENCAPSULATED
+    // BINDING OF DATA AND METHOD
+    // HIDING OF DATA
+    private String name;//name of variable starts with lower cases character, if more than one words
     // all consecutive words will start with uppers case
     int age;
     String email;
-    long mobile ;
+    long mobile;
     String address;
+
+    //getter
+    public String getName() {
+        return name;
+    }
+
     //Address address;
-
-
+    //getter and setter
+    // accessor and mutator
+    //getter will help you in accessing private attribute or field
+    //setter will help you in updating or modifying private attribute
+    //setter
+    // public void setAttributeName( type and name) type -> similar to data
+    // type of attribute and name can be anything, convention suggest,
+    // name should be same as attribute
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/StudentMain.java
+++ src/StudentMain.java
@@ -16,7 +16,7 @@ public class StudentMain {
         //don't know --> size we don't --> we are assuming 100 student data we will get
         // max size as 100 -> the size of student array is 100
         //count of student object
-        final int MAX_SIZE = 100;
+        final int MAX_SIZE = 1;
         Scanner scanner = new Scanner(System.in);
 
         // type[] nameOfArray = new type[int size];
@@ -35,9 +35,12 @@ public class StudentMain {
             switch (choice){
                 case 1: // do it by calling a method to preform case 1
                         // do it here -- using this way
-                    Student object = new Student();
+                    /*Student object = new Student();
                     System.out.println("enter name");
-                    object.name = scanner.nextLine(); //vaibhav diwan
+                    //updating the value of name
+
+                    String name = scanner.nextLine(); //vaibhav diwan
+                    object.setName(name);
                     System.out.println("enter age");
                     object.age = scanner.nextInt();
                     scanner.nextLine();
@@ -50,8 +53,30 @@ public class StudentMain {
                     object.address = scanner.nextLine();
 
                     //assign the object to array
-                    students[count] = object;
-                    count ++;
+                    students[count] = object;*/
+                    if (count != MAX_SIZE) {
+                        students[count] = new Student();
+                        //students[count].setName(scanner.nextLine());
+                        //Student obj = students[count];
+                        //String name = scanner.nextLine();
+                        System.out.println("enter name");
+                        students[count].setName(scanner.nextLine());
+                        System.out.println("enter age");
+                        students[count].age = scanner.nextInt();
+                        scanner.nextLine();//skipping \n
+                        System.out.println("enter email");
+                        students[count].email = scanner.nextLine();
+                        System.out.println("enter mobile");
+                        students[count].mobile = scanner.nextLong();
+                        scanner.nextLine();
+                        System.out.println("enter address");
+                        students[count].address = scanner.nextLine();
+                        count++;
+                    } else {
+                        System.out.println("no more space to store details");
+                        //System.exit(0);
+                        //choice = 3;
+                    }
                     break;
                 case 2:
                     //display all the element of student array
@@ -67,10 +92,11 @@ public class StudentMain {
 
         } while (choice != 3);
     }
-    public static void display(Student[] array,int noOfObjects){
+
+    public static void display(Student[] array, int noOfObjects) {
         for (int i = 0; i < noOfObjects; i++) {
             Student object = array[i];
-            System.out.println("object.name = " + object.name);
+            System.out.println("object.name = " + object.getName());
             System.out.println("object.age = " + object.age);
             System.out.println("object.email = " + object.email);
             System.out.println("object.mobile = " + object.mobile);
